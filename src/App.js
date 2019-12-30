a4c87c79ebcc8921c831d30bb52ec826a6f35f2c
em.es <em.es@kakaoenterprise.com>
2019-12-30 15:32:23 +0900

    tear down from inhouse repo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Howl } from 'howler';
+import startSoundSrc from "./start-recording.mp3";
+import stopSoundSrc from "./stop-recording.mp3";
 
-function App() {
+import MediaRecorder from 'opus-media-recorder';
+
+const startSound = new Howl({src: startSoundSrc});
+const stopSound = new Howl({src: stopSoundSrc});
+
+const workerOptions = {
+  encoderWorkerFactory: function () {
+    return new Worker(process.env.PUBLIC_URL + '/opus-media-recorder/encoderWorker.umd.js')
+  },
+  OggOpusEncoderWasmPath: process.env.PUBLIC_URL + '/opus-media-recorder/OggOpusEncoder.wasm',
+  WebMOpusEncoderWasmPath: process.env.PUBLIC_URL + '/opus-media-recorder/WebMOpusEncoder.wasm',
+};
+
+export default () => {
+  const [recordings, setRecordings] = useState([]);
+
+  const rec = () => {
+    navigator.mediaDevices.getUserMedia({audio: true}).then(stream => {
+      const options = { mimeType: 'audio/ogg' };
+      const recorder = new MediaRecorder(stream, options, workerOptions);
+      recorder.addEventListener('dataavailable', e => {
+        setRecordings(recordings.concat([{url: URL.createObjectURL(e.data)}]));
+        stream.getTracks().forEach(function(track) {
+          track.stop();
+        });
+      });
+      recorder.start();
+      startSound.once("end", () => {
+        setTimeout(() => {
+          recorder.stop();
+          stopSound.play();
+        }, 1000);
+      });
+      startSound.play();
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={rec}>Recording 1 seconds</button>
+      {recordings.map(recording => {
+        return <div key={recording.url}><audio controls src={recording.url} /></div>
+      })}
     </div>
   );
 }
 
-export default App;
